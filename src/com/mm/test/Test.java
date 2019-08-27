73c2f3fc853d1a46c1235d866fd65a000d3e146d
snow1985 <53985868+snow1985@users.noreply.github.com>
2019-08-27 15:43:17 +0800

    Initial commit

---
+++ src/com/mm/test/Test.java
@@ -0,0 +1,17 @@
+package com.mm.test;
+
+
+import java.time.Clock;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        List<Integer> list=new ArrayList<>();
+        System.out.println("hello world!");
+             System.out.println();
+
+       
+
+    }
+}
